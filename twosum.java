407f7c696f21e54394a330cadfd7175a31c89fe0
Dylan Tran <75633077+DylanBT928@users.noreply.github.com>
2023-06-02 22:07:16 -0700

    Create twosum.java

---
+++ twosum.java
@@ -0,0 +1,20 @@
+/*
+(1) Two-Sum
+https://leetcode.com/problems/two-sum/description/
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int x = 0, y = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i; j < nums.length; j++){
+                if(nums[i]+nums[j]==target && i!=j){
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        int[] answer = {x,y};
+        return answer;
+    }
+}
