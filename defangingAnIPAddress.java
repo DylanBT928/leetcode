f903d53643722ef916693535f51919da4b6652b0
Dylan Tran <75633077+DylanBT928@users.noreply.github.com>
2023-06-02 22:11:33 -0700

    Create defangingAnIPAddress.java

---
+++ defangingAnIPAddress.java
@@ -0,0 +1,18 @@
+/*
+(1108) Defanging an IP Address
+https://leetcode.com/problems/defanging-an-ip-address/description/
+*/
+
+class Solution {
+    public String defangIPaddr(String address) {
+        String str = "";
+        for(int i = 0; i<address.length(); i++){
+            if(address.substring(i,i+1).equals(".")){
+                str+="[.]";
+            } else {
+                str+=address.substring(i,i+1);
+            }
+        }
+        return str;
+    }
+}
