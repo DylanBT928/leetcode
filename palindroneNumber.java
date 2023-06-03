55e1ec6940c1f1c5ca794077b666c0ff70cab2af
Dylan Tran <75633077+DylanBT928@users.noreply.github.com>
2023-06-02 22:09:13 -0700

    Create palindroneNumber.java

---
+++ palindroneNumber.java
@@ -0,0 +1,15 @@
+/*
+(9) Palindrome Number
+https://leetcode.com/problems/palindrome-number/description/
+*/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        String str2 = "";
+        for(int i = str.length(); i>0; i--){
+            str2+=str.substring(i-1,i);
+        }
+        return str.equals(str2);
+    }
+}
