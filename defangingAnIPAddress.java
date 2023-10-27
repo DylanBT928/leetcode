a5cda0cf681a70534a133e0caf76d666b810d4da
Dylan Tran <dylanbt928@gmail.com>
2023-10-27 10:20:08 -0700

    Update defangingAnIPAddress.java

--- defangingAnIPAddress.java
+++ defangingAnIPAddress.java
@@ -1,6 +1,6 @@
 /*
 (1108) Defanging an IP Address
-https://leetcode.com/problems/defanging-an-ip-address/description/
+https://leetcode.com/problems/defanging-an-ip-address/
 */
 
 class Solution {
