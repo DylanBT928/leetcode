fdcea619f12956b35bfc4536d06e2cadf1001b14
Dylan Tran <dylanbt928@gmail.com>
2023-10-27 10:11:23 -0700

    Create returnLengthOfArgumentsPassed.js

---
+++ returnLengthOfArgumentsPassed.js
@@ -0,0 +1,8 @@
+/*
+(2703) Return Length of Arguments Passed
+https://leetcode.com/problems/return-length-of-arguments-passed/description/
+*/
+
+var argumentsLength = function(...args) {
+    return args.length;
+};
