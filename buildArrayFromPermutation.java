36b0a153a25e7e7c3b43aed09d1d673b2bc5ef00
Dylan Tran <dylanbt928@gmail.com>
2023-10-27 10:19:54 -0700

    Update buildArrayFromPermutation.java

--- buildArrayFromPermutation.java
+++ buildArrayFromPermutation.java
@@ -1,6 +1,6 @@
 /*
 (1920) Build Array from Permutation
-https://leetcode.com/problems/build-array-from-permutation/description/
+https://leetcode.com/problems/build-array-from-permutation/
 */
 
 class Solution {
